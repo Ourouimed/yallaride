355c59427a7af8453a763a542301a8c225df3d28
Mohamed Amine Ouroui <medtube835@gmail.com>
2025-11-01 22:42:13 +0100

    Create a breadcrumb list in dashboardLayout

--- app/dashboard/dashboardLayout.jsx
+++ app/dashboard/dashboardLayout.jsx
@@ -1,15 +1,20 @@
 'use client'
 import { AppSidebar } from "@/components/app-sidebar"
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb"
 import { Separator } from "@/components/ui/separator"
 import { SidebarInset, SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { useAuth } from "@/context/AuthContext"
-import { useRouter } from "next/navigation"
+import Link from "next/link"
+import { usePathname, useRouter } from "next/navigation"
 import { useEffect, useState } from "react"
 
 export default function DashboardLayout({ children }) {
   const router = useRouter()
   const { user, isLoading } = useAuth()
   const [mounted, setMounted] = useState(false)
+  const pathname = usePathname()
+  const segments = pathname.split('/').filter(Boolean)
+
 
   
   useEffect(() => {
@@ -40,6 +45,36 @@ export default function DashboardLayout({ children }) {
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
             <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4" />
+            <Breadcrumb>
+              <BreadcrumbList>
+                <BreadcrumbItem>
+                  <BreadcrumbLink asChild>
+                    <Link href="/dashboard">Dashboard</Link>
+                  </BreadcrumbLink>
+                </BreadcrumbItem>
+
+                {segments.filter((_,i)=> i!== 0).map((segment, index) => {
+                  const href = '/' + segments.slice(0, index + 1).join('/')
+                  const isLast = index === segments.length - 1
+                  const label = segment.charAt(0).toUpperCase() + segment.slice(1)
+
+                  return (
+                    <div key={href} className="flex items-center">
+                      <BreadcrumbSeparator />
+                      <BreadcrumbItem>
+                        {isLast ? (
+                          <span className="text-muted-foreground">{label}</span>
+                        ) : (
+                          <BreadcrumbLink asChild>
+                            <Link href={href}>{label}</Link>
+                          </BreadcrumbLink>
+                        )}
+                      </BreadcrumbItem>
+                    </div>
+                  )
+                })}
+              </BreadcrumbList>
+            </Breadcrumb>
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
